f15b998d7a4e4b89bab70f65131129a45e9d97e9
AlokikPathak <alokik2009@mindfiresolutions.com>
2019-11-09 03:12:43 +0530

    analyse json structure

--- parseDdrSummary.js
+++ parseDdrSummary.js
@@ -6,90 +6,28 @@
  /**
   * {
 	"FMPReport": {
-		"@attributes": {
-			"creationTime": "6:32:38 AM",
-			"creationDate": "11/6/2019",
-			"type": "Summary",
-			"version": "17.0.1"
+		"@attributes": { "creationTime": "6:32:38 AM","creationDate": "11/6/2019","type": "Summary","version": "17.0.1"
 		},
 		"#text": ["\n\t", "\n"],
 		"File": {
-			"@attributes": {
-				"link": ".//CFS Centrepoint_fmp12.xml",
-				"name": "CFS Centrepoint.fmp12",
-				"path": "E:\\CentreForSight_31_05_\\"
+			"@attributes": {"link": ".//CFS Centrepoint_fmp12.xml","name": "CFS Centrepoint.fmp12","path": "E:\\CentreForSight_31_05_\\"
 			},
 			"#text": ["\n\t\t", "\n\t\t", "\n\t\t", "\n\t\t", "\n\t\t", "\n\t\t", "\n\t\t", "\n\t\t", "\n\t\t", "\n\t\t", "\n\t\t", "\n\t\t", "\n\t\t", "\n\t\t", "\n\t"],
-			"BaseTables": {
-				"@attributes": {
-					"count": "37"
-				}
+			"BaseTables": {"@attributes": {"count": "37"}
 			},
-			"Tables": {
-				"@attributes": {
-					"count": "304"
-				}
-			},
-			"Relationships": {
-				"@attributes": {
-					"count": "264"
-				}
-			},
-			"Accounts": {
-				"@attributes": {
-					"count": "11"
-				}
-			},
-			"Privileges": {
-				"@attributes": {
-					"count": "9"
-				}
-			},
-			"ExtendedPrivileges": {
-				"@attributes": {
-					"count": "11"
-				}
-			},
-			"FileAccess": {
-				"@attributes": {
-					"count": "0"
-				}
-			},
-			"Layouts": {
-				"@attributes": {
-					"count": "281"
-				}
-			},
-			"Scripts": {
-				"@attributes": {
-					"count": "780"
-				}
-			},
-			"ValueLists": {
-				"@attributes": {
-					"count": "156"
-				}
-			},
-			"CustomFunctions": {
-				"@attributes": {
-					"count": "14"
-				}
-			},
-			"FileReferences": {
-				"@attributes": {
-					"count": "1"
-				}
-			},
-			"CustomMenuSets": {
-				"@attributes": {
-					"count": "1"
-				}
-			},
-			"CustomMenus": {
-				"@attributes": {
-					"count": "32"
-				}
-			}
+			"Tables": {"@attributes": {"count": "304"}},
+			"Relationships": {"@attributes": {"count": "264"}},
+			"Accounts": {"@attributes": {"count": "11"}},
+			"Privileges": {"@attributes": {"count": "9"}},
+			"ExtendedPrivileges": {"@attributes": {"count": "11"}},
+			"FileAccess": {"@attributes": {"count": "0"}},
+			"Layouts": {"@attributes": {"count": "281"}},
+			"Scripts": {"@attributes": {"count": "780"}},
+			"ValueLists": {"@attributes": {"count": "156"}},
+			"CustomFunctions": {"@attributes": {"count": "14"}},
+			"FileReferences": {"@attributes": {"count": "1"}},
+			"CustomMenuSets": {"@attributes": {"count": "1"}},
+			"CustomMenus": {"@attributes": {"count": "32"}}
 		}
 	}
 }
@@ -101,6 +39,8 @@ function parseDdrSummary( summaryJson ){
     'CustomFunctions', 'FileReferences', 'CustomMenuSets', 'CustomMenus' ];
 
     var summaryArr = {}; // Stores JSON in associative array format
+
+
 
     
 }
